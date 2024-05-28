6998d89ddf0aac612de0cef211c0c3e4e1dd5eaf
VictorGabriel2022 <mazmorra13rol@gmail.com>
2024-05-28 16:51:47 -0500

    first commit

--- src/Index.jsx
+++ src/Index.jsx
@@ -3,14 +3,14 @@ function Index() {
   return (
     <div className="containerinfo">
   <div className="wrapper">
-      <img className="items items1" src="js.png" alt="JavaScript" />
-      <img src="css-3.png" className="items items2" alt="CSS3" />
-      <img src="html-5.png" className="items items3" alt="HTML5" />
-      <img src="github.png" className="items items4"alt="GitHub" />
-      <img src="science.png" className="items items5" alt="Science" />    
-      <img src="css-3.png" className="items items6" alt="CSS3" />
-       <img src="html-5.png" className="items items7" alt="HTML5" />
-        <img className="items items8" src="mysql.png" alt="MySQL" />
+      <img className="itemRight items1" src="js.png" alt="JavaScript" />
+      <img src="css-3.png" className="itemRight items2" alt="CSS3" />
+      <img src="html-5.png" className="itemRight items3" alt="HTML5" />
+      <img src="github.png" className="itemRight items4"alt="GitHub" />
+      <img src="science.png" className="itemRight items5" alt="Science" />    
+      <img src="css-3.png" className="itemRight items6" alt="CSS3" />
+       <img src="html-5.png" className="itemRight items7" alt="HTML5" />
+        <img className="itemRight items8" src="mysql.png" alt="MySQL" />
       </div>
       <div className="info">
         <object type="image/svg+xml" data="aaa.svg" height={300} />
@@ -30,14 +30,14 @@ function Index() {
         </div>
       </div>
       <div className="wrapper">
-      <img className="items items1" src="js.png" alt="JavaScript" />
-      <img src="css-3.png" className="items items2" alt="CSS3" />
-      <img src="html-5.png" className="items items3" alt="HTML5" />
-      <img src="github.png" className="items items4"alt="GitHub" />
-      <img src="science.png" className="items items5" alt="Science" />    
-      <img src="css-3.png" className="items items6" alt="CSS3" />
-       <img src="html-5.png" className="items items7" alt="HTML5" />
-        <img className="items items8" src="mysql.png" alt="MySQL" />
+      <img className="itemLeft items1" src="js.png" alt="JavaScript" />
+      <img src="css-3.png" className="itemLeft items2" alt="CSS3" />
+      <img src="html-5.png" className="itemLeft items3" alt="HTML5" />
+      <img src="github.png" className="itemLeft items4"alt="GitHub" />
+      <img src="science.png" className="itemLeft items5" alt="Science" />    
+      <img src="css-3.png" className="itemLeft items6" alt="CSS3" />
+       <img src="html-5.png" className="itemLeft items7" alt="HTML5" />
+        <img className="itemLeft items8" src="mysql.png" alt="MySQL" />
       </div>
     </div>   
   
